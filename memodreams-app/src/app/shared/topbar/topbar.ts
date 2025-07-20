1c97e9436e1808a1ac453099988faac0568f74a3
ivan.ortiz <ivantiz45@gmail.com>
2025-07-20 13:55:22 +0200

    feat(Topbar): :sparkles: Create new topbar

---
+++ memodreams-app/src/app/shared/topbar/topbar.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [
+    RouterModule,
+    MenubarModule,
+    ButtonModule,    
+  ],
+  templateUrl: './topbar.html',
+  styleUrls: ['./topbar.scss']
+})
+export class Topbar {
+  sidebarVisible = false;
+
+  menuItems: MenuItem[] = [
+    { label: 'INICIO', routerLink: '/' },
+    { label: 'SERVICIOS', routerLink: '/servicios' },
+    { label: 'EVENTOS', routerLink: '/eventos' },
+    { label: 'GALERÍA', routerLink: '/galeria' },
+    { label: 'CONTACTO', routerLink: '/contacto' }
+  ];
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  openSocialMedia(socialMedia: string) {
+    switch (socialMedia) {
+      case 'facebook':
+        window.open('https://www.facebook.com/memodreamsevents', '_blank');
+        break;
+      case 'instagram':
+        window.open('https://www.instagram.com/memodreamsevents', '_blank');
+        break;
+      case 'tiktok':
+        window.open('https://www.tiktok.com/@memodreamsevents', '_blank');
+        break;
+    }
+  }}
