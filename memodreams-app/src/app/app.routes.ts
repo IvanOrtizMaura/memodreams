af04c8086880932242785268d21c27394f64d2b7
IvanOrtizMaura <66464260+IvanOrtizMaura@users.noreply.github.com>
2025-07-21 01:22:26 +0200

    feat(WIP MDreams): :sparkles: Create the work in progress page (#4)

--- memodreams-app/src/app/app.routes.ts
+++ memodreams-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { WorkInProgressComponent } from './shared/work-in-progress/work-in-progress.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: WorkInProgressComponent },
+  { path: 'servicios', component: WorkInProgressComponent },
+  { path: 'galeria', component: WorkInProgressComponent },
+  { path: 'eventos', component: WorkInProgressComponent },
+  { path: 'contacto', component: WorkInProgressComponent },
+  { path: '**', redirectTo: '/inicio' }
+];
