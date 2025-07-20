5a6bcfe5025c4eedf9b23b86bb6ca6d6a948308c
ivan.ortiz <ivantiz45@gmail.com>
2025-07-21 01:04:23 +0200

    feat(WIP MDreams): :sparkles: add a work in progress page

---
+++ memodreams-app/src/app/shared/work-in-progress/work-in-progress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-in-progress',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-in-progress.component.html',
+  styleUrls: ['./work-in-progress.component.scss']
+})
+export class WorkInProgressComponent {
+
+}
